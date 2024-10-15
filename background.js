422963fb95c9fd552dad43c121ae586c546f4825
ecsd-gituser <ecsd.github@eagleschools.net>
2024-10-15 09:33:02 -0600

    Add files via upload

---
+++ background.js
@@ -0,0 +1,7 @@
+// background.js for OTP Display App
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('OTP Display App installed');
+});
+
+// You can add more background logic here if needed
