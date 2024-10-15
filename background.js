eeea000501b07ba6ed3f3f4c47202e1d39bdaf5c
ecsd-gituser <ecsd.github@eagleschools.net>
2024-10-15 09:54:00 -0600

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-// background.js for OTP Display App
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('OTP Display App installed');
-});
-
-// You can add more background logic here if needed
